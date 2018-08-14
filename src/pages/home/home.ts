392c57cdb5052a9a6c015acaf23ef8f0ac801c26
nokuthula <novembernokuthula9@gmail.com>
2018-08-14 10:30:43 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,97 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController, Platform } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Flashlight } from '@ionic-native/flashlight';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+selector: 'page-home',
+templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  isOn:boolean
+public photos : any;
+public base64Image : string;
 
-  }
+constructor(public navCtrl: NavController,private camera: Camera,private alertCtrl : AlertController,private flash:Flashlight, private platform:Platform) {
+  this.platform.ready().then(()=>{
 
+  })
 }
+switchon():void{
+  this.flash.switchOn();
+  this.updateflashlight();
+}
+switchoff():void{
+  this.flash.switchOn();
+  this.updateflashlight();
+}
+updateflashlight(){
+  this.isOn= this.flash.isSwitchedOn();
+}
+
+ngOnInit() {
+ this.photos = [];
+}
+
+
+takePic(){
+ const options : CameraOptions = {
+   quality: 100, // picture quality
+   destinationType: this.camera.DestinationType.DATA_URL,
+   encodingType: this.camera.EncodingType.JPEG,
+   mediaType: this.camera.MediaType.PICTURE,
+   correctOrientation:true,
+     saveToPhotoAlbum:true
+
+ }
+
+ this.camera.getPicture(options) .then((imageData) => {
+   this.base64Image = "data:image/jpeg;base64," + imageData;
+   this.photos.push(this.base64Image);
+   this.photos.reverse();
+ }, (err) => {
+   console.log(err);
+ });
+}
+deletePhoto(index) {
+this.photos.splice(index, 1);
+let confirm =this.alertCtrl.create({
+title: 'wanna delete pic?',
+message:'',
+buttons:[
+ {
+   text: 'No',
+   handler: () => {
+     console.log('Disagree clicked');
+   }
+ },{
+   text: 'Yes',
+   handler: () =>{
+     console.log('Agree clicked');
+         this.photos.splice(index, 1);
+   }
+ }
+]
+});
+confirm.present();
+}
+Gallery(){
+  const options : CameraOptions = {
+    quality: 100, // picture quality
+    destinationType: this.camera.DestinationType.DATA_URL,
+    sourceType:this.camera.PictureSourceType.PHOTOLIBRARY,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    correctOrientation:true,
+    saveToPhotoAlbum:true
+  }
+  this.camera.getPicture(options) .then((imageData) => {
+    this.base64Image = "data:image/jpeg;base64," + imageData;
+    this.photos.push(this.base64Image);
+    this.photos.reverse();
+  }, (err) => {
+    console.log(err);
+  });
+  }
+  }
